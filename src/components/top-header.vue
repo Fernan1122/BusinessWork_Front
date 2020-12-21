78d1bd24a5b1640aabf0defb9d9e7b216a32df2d
Fernan1122 <60754935+Fernan1122@users.noreply.github.com>
2020-12-21 18:35:50 -0500

    prueba 3 heroku

--- src/components/top-header.vue
+++ src/components/top-header.vue
@@ -2,12 +2,8 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand id="titulo" to="/">BusinessWork</b-navbar-brand>
-
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
       <b-collapse id="nav-collapse" is-nav>
-
-        <!-- Right aligned nav items -->
         <b-navbar-nav id="buscador">
           <b-nav-form @submit.prevent="search" class="buscador2">
             <b-button size="sm" class="my-2 my-sm-0" type="submit" style="background-color: white;"
@@ -18,22 +14,6 @@
               v-model="searchText"
             ></b-form-input>
           </b-nav-form>
-
-          <!-- <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>-->
-
-          <!-- <b-nav-item-dropdown right> -->
-          <!-- Using 'button-content' slot -->
-          <!-- <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown> -->
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item class="headericon" to="/agregar"><img id="anadir" src="../assets/anadir.svg" width="30rem" height="30rem"/></b-nav-item>
